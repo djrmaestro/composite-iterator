27d3d1d4098efa4d94fdafa4886688d8bb0dc4b6
Kurt Krueckeberg <kurtk@pobox.com>
2013-12-27 19:16:03 -0600

    removed cast_const from Recursive()

--- Directory.h
+++ Directory.h
@@ -151,7 +151,7 @@ template<typename F> void Directory::DoRecursive(F& func, const Directory *pdir,
 }
 template<typename F> void Directory::Recursive(F& f) 
 {
-   this->DoRecursive(f, const_cast<Directory *>(this), std::string("")); // const_cast ist a hack
+   this->DoRecursive(f, this, std::string("")); // const_cast ist a hack
 }
 
 inline Node &Directory::DirectoryIterator::operator *() const
