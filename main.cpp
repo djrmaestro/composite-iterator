ddae9124aecda566bbb2e4c98f1edc211a02a9c5
Kurt Krueckeberg <kurtk@pobox.com>
2013-12-30 16:47:46 -0600

    converted all of main.cpp to use mkdir() to create directories

--- main.cpp
+++ main.cpp
@@ -7,40 +7,10 @@
 #include <iostream>
 
 using namespace std;
-/*
-void test_driver(const string& path)
-{
-   string sub_path = subpath(path); 
-   cout << " subpath(" << path << ") is: " << sub_path << endl;
 
-   if (sub_path.empty()) {
-
-        // The time is hardcode for now
-        cout << "Directory *pDir = new Directory(" << path << ", 10/10/2013)" << endl; 
-             
-   } else {
-
-      string name = head(path); 
-
-      cout << " head(" << path << ") is: " << name << endl;
-      
-      cout << "============================================================" << endl;
-
-      test_driver(sub_path);
-     
-   }
-}
-*/
 int main(int argc, char** argv) 
 {
-
-/*
-string test("subdirA/subdirB/subdirC/subdirD/newDir");
-
-   cout << "test string: " << test << endl << endl;
-   test_driver(test);
-  */ 
-        
+       
     try {
         
         File *ptop_f1 = new File(string("top-File1"), string("12-12-2013"));
@@ -54,11 +24,12 @@ string test("subdirA/subdirB/subdirC/subdirD/newDir");
         
         mkdir(&top, "subdir-mid");
         
-        // get the subdirectory we just made.
+        // get a pointer to the subdirectory we just made.
         Node *psubdir_mid = top.getChild(2);
         
         mkdir(&top, "subdir-mid/subdir-lower1");
         
+        // get a pointer to the subdirectory we just made. It is the first child of subir-mid/
         Node *psubdir_lower1 = psubdir_mid->getChild(0);
 
         File  *psubdir_lower1_f1 = new File(string("subdir-lower1-File1"), string("12-12-2013"));
@@ -69,69 +40,43 @@ string test("subdirA/subdirB/subdirC/subdirD/newDir");
         psubdir_lower1->adopt(psubdir_lower1_f2);
         
         File  *psubdir_mid_f1 = new File(string("subdir_mid-File1"), string("12-12-2013"));
-        
+    
         File  *psubdir_mid_f2 = new File(string("subdir-mid_File2"), string("12-10-2013"));
      
         psubdir_mid->adopt(psubdir_mid_f1);
         psubdir_mid->adopt(psubdir_mid_f2);
 
-                    
-        /*
-        Directory *psubdir_mid = new Directory(string("subdir-mid"), string("12-10-2013"));
-        
-        Directory *psubdir_lower1 = new Directory(string("subdir-lower1"), string("12-10-2013"));
-        
-        File  *psubdir_lower1_f1 = new File(string("subdir-lower1-File1"), string("12-12-2013"));
-        
-        File  *psubdir_lower1_f2 = new File(string("subdir-lower1-File2"), string("12-10-2013"));
-        
-        psubdir_lower1->adopt(psubdir_lower1_f1);
-        psubdir_lower1->adopt(psubdir_lower1_f2);
-              
-        psubdir_mid->adopt(psubdir_lower1);
-   
-        File  *psubdir_mid_f1 = new File(string("subdir_mid-File1"), string("12-12-2013"));
-        
-        File  *psubdir_mid_f2 = new File(string("subdir-mid_File2"), string("12-10-2013"));
-     
-        psubdir_mid->adopt(psubdir_mid_f1);
-        psubdir_mid->adopt(psubdir_mid_f2);
-          
-        // Code above this line has beenconverted to use mkdir, Code below has not been converted.
-        //TODO: Convert code below to use mkdir/getChild like above. 
-          
-        Directory *psubdir_lower2 = new Directory(string("subdir-lower2"), string("12-10-2013"));
-        
+        mkdir(&top, "subdir-mid/subdir-lower2");
+
+        // The subdirectory subdir-lower2/ is the third child (we index starting at zero) of its immediate parent subdir-mid/
+        Node *psubdir_lower2 = psubdir_mid->getChild(3);
+
         File  *psubdir_lower2_f1 = new File(string("subdir-lower2-File1"), string("12-12-2013"));
         
         File  *psubdir_lower2_f2 = new File(string("subdir-lower2-File2"), string("12-10-2013"));
         
         psubdir_lower2->adopt(psubdir_lower2_f1);
         psubdir_lower2->adopt(psubdir_lower2_f2);
-        
-        psubdir_mid->adopt(psubdir_lower2);
-   
-        top.adopt(psubdir_mid);
-        
+              
         File *ptop_f3 = new File(string("top-File3"), string("12-12-2013"));
         
         File *ptop_f4  = new File(string("top-File4"), string("12-10-2013"));
         
         top.adopt(ptop_f3);
-        top.adopt(ptop_f3);
+        top.adopt(ptop_f4);
         
-        cout << "\n ===== print recursively: top.Recursive(printer) =========== \n" << endl;
-        */ 
+        cout << "\n ===== top.traverse(CompositePrint(cout))  =========== \n" << endl;
+         
         CompositePrinter printer(cout);
 
         top.traverse(printer);        
                 
         cout << "\n ===== print using iterator: top.print() =========== \n" << endl;
 
-        top.print(); // print top level directory and its children
+        top.print(); 
         
-        cout << "\n ============= \n" << endl;
-
+        cout << "\n ====== using external iterators to print ==== \n" << endl;
+        
         Directory::iterator iter_current = top.begin();
         Directory::iterator iter_end = top.end();
 
@@ -178,7 +123,7 @@ string test("subdirA/subdirB/subdirC/subdirD/newDir");
               cout << endl;
         }
             
-    } catch (Node::node_logic_error& e) {
+    } catch (std::exception& e) {
             
           cout <<  e.what();
     }
